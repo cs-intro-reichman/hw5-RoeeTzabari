3899c8d8826c037c561b87b4e74978f04b92502e
Roee Tzabari <roee.tzabari@post.runi.ac.il>
2024-12-07 20:01:35 +0200

    Final

--- Scrabble.java
+++ Scrabble.java
@@ -106,6 +106,7 @@ public class Scrabble {
 			if (isWordInDictionary(input) && MyString.subsetOf(input, hand)) {
 				score += wordScore(input);
 				System.out.println(input + " earned " + wordScore(input) + " points." +  " Score: " + score + " points");
+				System.out.println();
 				hand = MyString.remove(hand, input);
 			}
 			else if (input.equals(".")) break;
